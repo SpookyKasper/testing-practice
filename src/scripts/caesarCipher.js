6d10ea807565b7681ce17bb7f8ba30b1b7d4ae85
Kasper Bak <102696643+SpookyKasper@users.noreply.github.com>
2024-07-19 23:29:41 +0200

    Implement a fully functioning and good looking caesarCipher TDD style

--- src/scripts/caesarCipher.js
+++ src/scripts/caesarCipher.js
@@ -2,23 +2,24 @@ import _ from "lodash";
 
 export default function caesarCipher(string, factor) {
   const splittedString = string.split("");
-  const cipheredArray = splittedString.map((element) => {
-    if (!isLetter(element)) {
-      return element;
-    }
-    const charCode = element.charCodeAt(0);
-    const charCodePlusFactor = charCode + factor;
-    const min = isLowerCaseLetter(charCode) ? 97 : 65;
-    const max = isLowerCaseLetter(charCode) ? 122 : 90;
-    const wrappedCharCode = dealWithOutOfRange(charCodePlusFactor, min, max);
-    const convertToChar = String.fromCharCode(wrappedCharCode);
-    return convertToChar;
+  const cipheredArray = splittedString.map((char) => {
+    return isLetter(char) ? cipherLetter(char, factor) : char;
   });
   const joinedString = cipheredArray.join("");
   return joinedString;
 }
 
-const dealWithOutOfRange = (charCode, min, max) => {
+const cipherLetter = (char, factor) => {
+  const charCode = char.charCodeAt(0);
+  const charCodePlusFactor = charCode + factor;
+  const min = isLowerCaseLetter(charCode) ? 97 : 65;
+  const max = isLowerCaseLetter(charCode) ? 122 : 90;
+  const wrappedCharCode = wrapCharCode(charCodePlusFactor, min, max);
+  const convertToChar = String.fromCharCode(wrappedCharCode);
+  return convertToChar;
+};
+
+const wrapCharCode = (charCode, min, max) => {
   if (charCode > max) {
     charCode -= 26;
   } else if (charCode < min) {
